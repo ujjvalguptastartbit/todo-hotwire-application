3485353f82ff39bbad3978edddc8dfe9eb1572ea
ujjvalguptastartbit <ujjvalgupta@startbitsolutions.com>
2023-10-25 11:03:33 +0530

    controller active record added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-
+	before_action :allow_cross_domain_access
 
 	def allow_cross_domain_access
     	response.headers["Access-Control-Allow-Origin"] = '*'
     	response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE, OPTIONS"
     	response.headers['Access-Control-Request-Method'] = '*'
     	response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+
   	end
 end
